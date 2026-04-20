2b9036650e6f5357eb9ec4841724d45b06e383e3
Saswat <saswat1091samal@gmail.com>
2026-04-20 15:02:04 +0530

    implementation of the hash and test file

---
+++ better_hash/test.c
@@ -0,0 +1,65 @@
+#include "hashtable.h"
+#include <time.h>
+
+#define MAX_LINE 4096
+
+uint64_t hash(const char *name, size_t length)
+{
+    uint64_t hash_value = 0;
+    for(int i = 0; i < length; i++) {
+        hash_value += name[i];
+        hash_value = hash_value * name[i];
+    }
+    return hash_value;
+}
+
+void generate_random_word(char *buffer, size_t length)
+{
+    for(size_t i = 0; i < length-1; i++) {
+        buffer[i] = 'a' + (rand()%26);
+    }
+    buffer[length-1] = 0;
+}
+
+int main(int argc, char **argv) 
+{
+    srand(time(NULL));
+    if(argc != 3) {
+        printf("usage: %s <wordlist filename> <num guesses>\n");
+        return EXIT_FAILURE;
+    }
+
+    char *filename = argv[1];
+    uint32_t num_guesses = atol(argv[2]);
+
+    const int tablesize = (1<<20);
+    hash_table *table = hash_table_create(tablesize, hash);
+
+    FILE *fp = fopen(filename, "r");
+    char buffer[MAX_LINE];
+    uint32_t numwords = 0;
+    while (!feof(fp) && fgets(buffer, MAX_LINE, fp) != NULL) {
+        buffer[strcspn(buffer, "\n\r")] = 0;
+        char *newentry = malloc(strlen(buffer) + 1);
+        strcpy(newentry, buffer);
+        hash_table_insert(table, newentry, newentry);
+        numwords++;
+    }
+    fclose(fp);
+    printf("Loaded %d words into the table\n", numwords);
+    // hash_table_print(table);
+
+    // now make guesses
+    uint32_t good_guesses = 0;
+    const int shortest_guess = 2;
+    const int longest_guess = 20;
+    for(uint32_t i = 0; i < num_guesses; i++) {
+        generate_random_word(buffer, shortest_guess + (rand() % (longest_guess-shortest_guess+1)));
+        if(hash_table_lookup(table, buffer)) {
+            printf("%s\n", buffer);
+            good_guesses++;
+        }
+    }
+    printf("%u out of %u guesses were in table\n", good_guesses, num_guesses);
+    hash_table_destroy(table);
+}
